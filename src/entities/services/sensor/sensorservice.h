07174b75731669020690443f8324c66c2bddabf1
André Luiz <alps2@cin.ufpe.br>
2022-01-24 19:14:03 -0300

    Doc for Sensor Service

--- src/entities/services/sensor/sensorservice.h
+++ src/entities/services/sensor/sensorservice.h
@@ -11,8 +11,21 @@ public:
     QString name();
 
     QMutex mutex;
-
+    /**
+     * Updates the status of a single robot.
+     *
+     * @param The new RobotStatus (containing the identifier)
+     * @param request Empty as default (no request data needed)
+     * @return status of the request
+     */
     grpc::Status SetRobotStatus(grpc::ServerContext* context, RobotStatus* robotStatus, const ::google::protobuf::Empty* request);
+    /**
+     * Updates the status of many robots.
+     *
+     * @param The new RobotStatus (containing the identifiers)
+     * @param request Empty as default (no request data needed)
+     * @return status of the request
+     */
     grpc::Status SetAllRobotStatus(grpc::ServerContext* context, grpc::ServerReader<RobotStatus>* allRobotStatus, const ::google::protobuf::Empty* request);
 private:
     QList<Robot> robots;
